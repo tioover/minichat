c741c339d4d62559c50196ca1c8f6c910772ca1b
tioover <tioover@gmail.com>
2011-12-13 02:37:55 +0800

    orz

--- static/main.js
+++ static/main.js
@@ -13,9 +13,8 @@ $(document).ready(function(){
 var item = {
     "update": function(sence){
         //长轮询加载item
-        var data = {sence:sence};
-        if (sence == 0) data={}
-        $.get("items.json",data,function(json_str){
+        var data = {sence: sence};
+        $.get("items.json", data, function(json_str){
             var items = JSON.parse(json_str);
             show.template_update(items);
             item.update(items[0].id); //递归调用，进行下一次长轮询
@@ -33,7 +32,7 @@ var item = {
             $.post("add/",post_data,function(){
                 $("#content").val(""); //清空输入框
             });
-        }
+        };
     },
 }
 
@@ -43,12 +42,12 @@ var show = {
         if ($("#name").val() == "") $("#name").focus();
         else if ($("#email").val() == "") $("email").focus();
         else if ($("#content").val() == "") $("#content").focus();
-        else return true
+        else return true;
     },
     
     "template_update": function(items){
         //使用jQuery template来更新HTML,模板内容见../template/home.html 头部.
-        $("#itemsTemplate").tmpl(items).prependTo("#list");
+        $("#templ").tmpl(items).prependTo("#list");
         $(".item").slideDown();
     },
 }
